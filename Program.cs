2b80966e470d52e684a9267357086c73578f5af8
karlphysic <physickarl@gmail.com>
2022-10-15 10:06:23 -0500

    loopback actualizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace universidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
